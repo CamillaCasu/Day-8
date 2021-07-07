c7eb32dec8de3998b59c43e1242111ba89418d4a
cam <camilla.casu@epitech.eu>
2021-07-08 00:24:14 +0200

    ex_01 à ex_06

---
+++ ex_05/index.php
@@ -0,0 +1,18 @@
+<?php
+$name = NULL;
+session_start();
+if (array_key_exists("name", $_GET)) {
+    $name = $_GET["name"];
+    $_SESSION["name"]=$name;
+}
+else if (array_key_exists("name", $_SESSION)) {
+    $name = $_SESSION["name"];
+}
+if ($name) {
+    echo "Hello ".$name;" !\n"
+}
+else {
+    echo "Hello platypus !\n";
+}
+setcookie("name", $name);
+?>
